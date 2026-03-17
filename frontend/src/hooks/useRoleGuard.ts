a6a4fc20f3f2e7c6e0e360df87d503783ce3e126
KennedyOti <otienokennedy972@gmail.com>
2026-03-17 22:45:14 +0300

    updates project

--- frontend/src/hooks/useRoleGuard.ts
+++
@@ -1,9 +0,0 @@
-export const useRoleGuard = (allowedRoles: string[]) => {
-//   const { user } = useContext(AuthContext);
-
-//   if (user && !allowedRoles.includes(user.role)) {
-//     return false;
-//   }
-
-  return true;
-};
